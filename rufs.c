76a59e8a5e931ae86a2766c7842581e8d9ff63c7
gane-sh <ganeshvankayala2@gmail.com>
2022-04-29 19:11:19 -0400

    Update rufs.c

--- rufs.c
+++ rufs.c
@@ -640,11 +640,13 @@ static int rufs_write(const char *path, const char *buffer, size_t size, off_t o
 	last_block_index = last_block_index < 16 ? last_block_index : 15;
 
 	char data_block[BLOCK_SIZE];
+	int bytes_copied = BLOCK_SIZE - offset;
 	if ((void *)inode->direct_ptr[first_block_index] == NULL) {
 		// Step 3: write the correct amount of data from offset to buffer
 
 		int avail_block = get_avail_blkno();
 		inode->direct_ptr[first_block_index] = (superblock.d_start_blk + avail_block) * BLOCK_SIZE;
+		memcpy(&data_block[offset], buffer, bytes_copied);
 		bio_write(inode->direct_ptr[first_block_index] / BLOCK_SIZE, &data_block);
 
 	}
@@ -654,17 +656,14 @@ static int rufs_write(const char *path, const char *buffer, size_t size, off_t o
 		return size;
 	}
 
-	int bytes_copied = BLOCK_SIZE - offset;
-	memcpy(&data_block[offset], buffer, bytes_copied);
-
 	for (int i = first_block_index + 1; i < last_block_index; i++) {
 		if ((void *)inode->direct_ptr[i] == NULL) {
 			int avail_block = get_avail_blkno();
 			inode->direct_ptr[i] = (superblock.d_start_blk + avail_block) * BLOCK_SIZE;
 			char data_block[BLOCK_SIZE];
+			memcpy(&data_block, buffer + bytes_copied, BLOCK_SIZE);
 			bio_write(data_block, inode->direct_ptr[i] / BLOCK_SIZE);
 			bytes_copied += BLOCK_SIZE;
-			memcpy(&data_block, buffer + bytes_copied, BLOCK_SIZE);
 		}
 	}
 	if ((void *)inode->direct_ptr[last_block_index] == NULL) {
